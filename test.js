04f1b19d4ed0f12c76c15492aef54c9ea62ba388
Ahmed_Nagy <ahmed.nagy.2002.an@gmail.com>
2023-05-27 22:11:48 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,32 @@
+//button.addEventListener('click', Unnamed1_Click())
+
+/*function toggleVisibility() {
+  var textBox = document.getElementById("myTextBox");
+  if (textBox.style.display === "none") {
+    textBox.style.display = "block";
+  } else {
+    textBox.style.display = "none";
+  }
+}*/
+
+
+/*app.post('/conjuring',function(){
+  let textBox = document.getElementById("myTextBox");
+  if (textBox.style.display === "none") {
+    textBox.style.display = "block";
+  } else {
+    textBox.style.display = "none";
+  }
+})*/
+
+/*const loginButton = document.querySelector('#login-button');
+
+
+loginButton.onclick = function(){
+
+    const h1 = document.querySelector("#myTextBox");
+    const underline = document.createElement("u");
+    underline.innerText = "Bye World!";
+    h1.innerText = "";
+    h1.append(underline);
+}*/
